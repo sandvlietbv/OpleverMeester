ddae05f4ab2a129382b1e84dcf2cf285b66158e2
sandvlietbv <sandvlietbv@gmail.com>
2026-08-30 19:50:51 +0200

    Clean up legacy indexing signal (#16)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/particulieren",
+        destination: "/woningontruiming",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
