28af8e9631c52d2b2b8aaf6460b0b22ba3ddad37
sandvlietbv <sandvlietbv@gmail.com>
2026-09-13 08:02:57 +0200

    Use www in robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,2 @@
 import type { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: "/api/",
-    },
-    sitemap: "https://oplevermeester.nl/sitemap.xml",
-    host: "https://oplevermeester.nl",
-  };
-}
+export default function robots(): MetadataRoute.Robots{return{rules:{userAgent:"*",allow:"/",disallow:"/api/"},sitemap:"https://www.oplevermeester.nl/sitemap.xml",host:"https://www.oplevermeester.nl"}}
